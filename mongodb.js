08cd8f7c86baf12dafa6b047a001d0bc70037e43
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-08-27 23:05:28 +0530

    obj_id-commit

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,19 @@
 //crud operation
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID
+// ObjectID class is imported from the mongodb package, and you can use it to generate new unique identifiers for documents. 
+
+// We can destructure the above code 
+const { MongoClient, ObjectID} = require("mongodb")
+
+const id = new ObjectID();
+console.log(id)
+console.log(id.getTimestamp())  // will return the date and time to milisec when the ObjectID was created
+console.log(id.id.length)
+console.log(id.toHexString())
+console.log(id.toHexString().length)
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -16,32 +28,33 @@ MongoClient.connect(
     console.log("Connected Correctly");
 
     const db = client.db(databaseName);
-    // db.collection('users').insertOne({
-    //     name: 'Amit',
-    //     age: 22
-    // }, (error, res)=>{
-    //     if(error)
-    //         return console.log("undable to insert")
-    //     return console.log(res.ops)
-    // })
-
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Mongo-InsertOne1",
-          completed: false,
-        },
-        {
-          description: "Mongo-InsertMany2",
-          completed: false,
-        },
-      ],
-      (err, res) => {
-        if(err){
-            return console.log("Error:", err);
-        }
+    db.collection('users').insertOne({
+        _id: id, //passing the id that we have created above
+        name: 'Subash',
+        age: 54
+    }, (error, res)=>{
+        if(error)
+            return console.log("undable to insert")
         return console.log(res.ops)
-      }
-    );
+    })
+
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "Mongo-InsertOne1",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "Mongo-InsertMany2",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (err, res) => {
+    //     if(err){
+    //         return console.log("Error:", err);
+    //     }
+    //     return console.log(res.ops)
+    //   }
+    // );
   }
 );
--- readme.md
+++ readme.md
@@ -14,3 +14,43 @@ res.ops - > will print the result in document format as it is stored in the coll
 ### for insert
 - its an async function
 - a Promise is returned if no callback is passed
+
+
+### ObjectID
+
+ObjectID class is imported from the mongodb package, and you can use it to generate new unique identifiers for documents.
+
+Each ObjectID is a 12-byte value consisting of a 4-byte timestamp, a 5-byte random value, and a 3-byte incrementing counter.
+
+eg: 
+```
+const id = new ObjectID();
+console.log(id)
+console.log(id.getTimestamp())  // will return the date and time to milisec when the ObjectID was created
+
+O/P:
+64eb885cd8eb3c03d8a6267a
+2023-08-27T17:31:08.000Z
+
+```
+**toHexString()**
+Return the ObjectID id as a 24 byte hex string representation
+
+eg:
+```
+const id = new ObjectID();
+console.log(id)
+console.log(id.getTimestamp())  // will return the date and time to milisec when the ObjectID was created
+console.log(id.id.length)
+console.log(id.toHexString())
+console.log(id.toHexString().length)
+
+O/P:
+64eb885cd8eb3c03d8a6267a
+2023-08-27T17:31:08.000Z
+12
+64eb885cd8eb3c03d8a6267a
+24
+```
+
+

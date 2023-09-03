d8f2a400116af382914c1fadbf93dec42ffee369
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-09-03 23:57:42 +0530

    init-commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/task-manager-api",
-  { useNewUrlParser: true }
-  ,
+  { useNewUrlParser: true, useCreateIndex: true },
   (error, client) => {
     if (error) {
       return console.log("Unable to connect to db");
@@ -11,17 +10,3 @@ mongoose.connect(
     console.log("Connected Correctly");
   }
 );
-
-//Task - Assignment
-// const Task = mongoose.model("Task", {
-//   description: {
-//     type: String, 
-//     required: true,
-//     trim: true
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false,
-
-//   }
-// })
--- src/index.js
+++ src/index.js
@@ -1,26 +1,68 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
+app.use(express.json());
 // going to automatically parse incoming JSON to an object, if not included, while accessing req.body will throw undefined message on console.
 
-app.post('/users',(req, res)=>{
-    console.log(req.body)
-    const user = new User(req.body)
-    user.save()
-    .then(()=>{
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err); //we can chain the methods together
+    });
+});
+
+app.get("/users", (req, res) => {
+  //finding all documents from collection
+  User.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+    //using request parameter
+    console.log(req.params)
+
+    User.findById(req.params.id)
+    .then((user)=>{
+        if(!user){
+            return res.status(404).send("Not Found!")
+        }
         res.send(user)
     })
     .catch(err=>{
-        res.status(400).send(err)   //we can chain the methods together
+        console.log('Inside Error')
+        res.status(500).send(err)
     })
 
 })
 
-app.listen(port, ()=>{
-    console.log('Server is up on port', port)
-})
+app.post("/task", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+      console.log(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log("Server is up on port", port);
+});

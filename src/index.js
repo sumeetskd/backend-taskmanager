fb57a2abb2d8fb72865548e03ce7e5097675af3b
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-09-10 02:38:02 +0530

    change to method

--- src/index.js
+++ src/index.js
@@ -8,81 +8,87 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 // going to automatically parse incoming JSON to an object, if not included, while accessing req.body will throw undefined message on console.
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err); //we can chain the methods together
-    });
+
+  try{
+    await user.save()
+    res.send(user)
+  }
+  catch(e){
+    res.status(400).send(e)
+  }
 });
 
-app.get("/users", (req, res) => {
-  //finding all documents from collection
-  User.find({})
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+
+  try{
+    const userData = await User.find({})
+    res.send(userData)
+  }
+  catch(e){
+    res.status(500).send()
+  }
+
 });
 
-app.get("/users/:id", (req, res) => {
-    //using request parameter
-    console.log(req.params)
+app.get("/users/:id", async (req, res) => {
+  //using request parameter
+  console.log(req.params);
+  try{
+    const userData = await User.findById(req.params.id)
+    if (!userData) {
+      return res.status(404).send("Not Found!");
+    }
+    res.send(userData);
+  }
+  catch(e){
+    console.log("Inside Error");
+    res.status(500).send(e);
+  }
+  
+});
 
-    User.findById(req.params.id)
-    .then((user)=>{
-        if(!user){
-            return res.status(404).send("Not Found!")
-        }
-        res.send(user)
-    })
-    .catch(err=>{
-        console.log('Inside Error')
-        res.status(500).send(err)
-    })
-
-})
-
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.send(task);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-      console.log(err);
-    });
+  try{
+    await task.save();
+    res.send(task);
+  }
+  catch(err){
+    res.status(400).send(err);
+    console.log(err);
+  }
+  
 });
 
-app.get("/tasks", (req, res)=>{
-    Task.find({})
-    .then((task)=>{
-        res.send(task)
-    })
-    .catch(e=>{
-        res.status(500).send()
-    })
-})
+app.get("/tasks", async (req, res) => {
 
-app.get("/tasks/:id", (req, res)=>{
-    console.log(req.params)
-    Task.findById(req.params.id)
-    .then((task)=>{
-        res.send(task)
-    })
-    .catch(e=>{
-        res.status(500).send()
-    })
-})
+  try{
+    const taskData = await Task.find({})
+    res.send(taskData)
+  }  
+  catch(err){
+    res.status(500).send(err);
+  }
+  
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  console.log(req.params);
+  try{
+    const taskData = await Task.findById(req.params.id)
+    if(!taskData){
+      res.send("ID is Not Found");
+    }
+    res.send(taskData)
+  }
+  catch(err){
+    res.status(500).send();
+  }
+  
+});
 
 app.listen(port, () => {
   console.log("Server is up on port", port);

f54b4acd3132f055ac7b8c5e98432f7a8ee1d0a1
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-09-13 12:22:45 +0530

    hashing-demo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
-// const route = new express.Router();
-// route.get('/test', (req, res)=>{
-//   res.send("Hello Test Router")
-// })
-// app.use(route)
 
 app.use(express.json());
 // going to automatically parse incoming JSON to an object, if not included, while accessing req.body will throw undefined message on console.
@@ -23,3 +17,15 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port", port);
 });
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async ()=>{
+  const password = "Sumeet14"
+  const hashedPassword = await bcrypt.hash(password, 8)
+  console.log(password)
+  console.log(hashedPassword)
+  const isMatched = await bcrypt.compare('Sumeet14', hashedPassword)
+  console.log(isMatched)
+}
+myFunction()
